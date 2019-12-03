3a22e179c1493ac7822674f042bcbf39e0c0dc2b
zlepper <hansen13579@gmail.com>
2019-12-03 08:58:20 +0100

    Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -20,9 +20,27 @@ impl AocImplementation<Vec<String>> for Day3 {
         let w2t = input[1].clone();
         let w2 = w2t.iter().map(|s| &s[..]).collect();
 
-        let distance = find_crossing_wires(w1, w2);
+//        let distance = find_crossing_wires(w1, w2);
+//        Some(distance)
+
+        let distance = find_first_crossing_point(w1, w2);
         Some(distance)
     }
+}
+
+fn find_first_crossing_point(wire1: Vec<&str>, wire2: Vec<&str>) -> i32 {
+    let wire1_coords = find_touched_coordinates(wire1);
+    let wire2_coords = find_touched_coordinates(wire2);
+
+    let intersections = find_intersections(wire1_coords.clone().into_iter().collect(), wire2_coords.clone().into_iter().collect());
+
+    let mut first_intersection: Vec<usize> = intersections.iter()
+        .map(|inter| wire1_coords.iter().position(|c| c == inter).unwrap() + wire2_coords.iter().position(|c| c == inter).unwrap() + 2)
+        .collect();
+
+    first_intersection.sort_by(|a, b| a.cmp(b));
+
+    first_intersection[0] as i32
 }
 
 // Calculates the distance to the closest intersection
@@ -30,7 +48,7 @@ fn find_crossing_wires(wire1: Vec<&str>, wire2: Vec<&str>) -> i32 {
     let wire1_coords = find_touched_coordinates(wire1);
     let wire2_coords = find_touched_coordinates(wire2);
 
-    let intersections = find_intersections(wire1_coords, wire2_coords);
+    let intersections = find_intersections(wire1_coords.into_iter().collect(), wire2_coords.into_iter().collect());
 
     let mut ordered: Vec<(i32, i32)> = intersections.into_iter().collect();
     ordered.sort_by(|a, b| calculate_manhattan_distance(a).cmp(&calculate_manhattan_distance(b)));
@@ -49,8 +67,8 @@ fn calculate_manhattan_distance((x, y): &(i32, i32)) -> i32 {
 }
 
 // Returns all the coordinates that this wire touches
-fn find_touched_coordinates(operations: Vec<&str>) -> HashSet<(i32, i32)> {
-    let mut coordinates = HashSet::new();
+fn find_touched_coordinates(operations: Vec<&str>) -> Vec<(i32, i32)> {
+    let mut coordinates = Vec::new();
     let mut current_position = (0, 0);
     for op in operations {
         let direction = &op[0..1];
@@ -65,7 +83,7 @@ fn find_touched_coordinates(operations: Vec<&str>) -> HashSet<(i32, i32)> {
 
         for _ in 0..distance {
             current_position = (current_position.0 + change.0, current_position.1 + change.1);
-            coordinates.insert(current_position);
+            coordinates.push(current_position);
         }
     }
 
@@ -83,7 +101,7 @@ mod tests {
 
         let coordinates = find_touched_coordinates(wire1);
 
-        let expected = vec![(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (7, 5), (6, 5), (5, 5), (4, 5), (3, 5), (3, 4), (3, 3), (3, 2)].into_iter().collect();
+        let expected = vec![(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (7, 5), (6, 5), (5, 5), (4, 5), (3, 5), (3, 4), (3, 3), (3, 2)];
 
 
         assert_eq!(coordinates, expected)
@@ -108,5 +126,11 @@ mod tests {
     fn example1() {
         let distance = find_crossing_wires(vec!["R8", "U5", "L5", "D3"], vec!["U7", "R6", "D4", "L4"]);
         assert_eq!(distance, 6);
+    }
+
+    #[test]
+    fn part1_example2() {
+        let distance = find_first_crossing_point("R75,D30,R83,U83,L12,D49,R71,U7,L72".split(',').collect(), "U62,R66,U55,R34,D71,R55,D58,R83".split(',').collect());
+        assert_eq!(distance, 610)
     }
 }
