5d44980042af5e6f92ea82dab0df4be6786fe510
Rasmus <hansen13579@gmail.com>
2019-12-09 18:08:59 +0100

    Day 8 - Part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,5 +1,6 @@
 use aoc_lib::AocImplementation;
 use itertools::Itertools;
+use image;
 
 fn main() {
     let day = Day8{};
@@ -14,16 +15,55 @@ impl AocImplementation<u8> for Day8 {
     }
 
     fn execute(&self, input: Vec<u8>) -> Option<i32> {
-        let layer: Vec<u8> = input.into_iter()
-            .chunks(25 * 6)
-            .into_iter()
-            .map(|l| l.collect::<Vec<u8>>())
-            .min_by_key(|l| l.iter().filter(|b| **b == 0).count())
-            .unwrap();
+        let width = 25;
+        let height = 6;
+        let layers = input.chunks(width * height).rev();
 
-        let digit1 = layer.iter().filter(|l| **l == 1).count();
-        let digit2 = layer.iter().filter(|l| **l == 2).count();
+        let mut img_buf = image::ImageBuffer::new(width as u32, height as u32);
 
-        Some((digit1 * digit2) as i32)
+        for layer in layers {
+            for (index, b) in layer.iter().enumerate() {
+                let (x, y) = get_coords_from_index(width, height, index);
+                println!("index: {}, x: {}, y: {}", index, x, y);
+                if *b == 2 { continue; }
+                let pixel = img_buf.get_pixel_mut(x, y);
+                *pixel = match b {
+                    0 => image::Rgb([0,0,0]),
+                    1 => image::Rgb([255,255,255]),
+                    _ => panic!("Unknown color: {}", b)
+                }
+            }
+        }
+
+        img_buf.save("code.png").unwrap();
+
+        Some(0)
     }
+}
+
+fn get_coords_from_index(width: usize, height: usize, index: usize) -> (u32, u32) {
+    let y = index / width;
+    let x = index % width;
+
+    (x as u32, y as u32)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_coordinates_0_0() {
+        let (x, y) = get_coords_from_index(25, 6, 0);
+        assert_eq!(x, 0);
+        assert_eq!(y, 0);
+    }
+
+    #[test]
+    fn correct_coordinates_25_6() {
+        let (x, y) = get_coords_from_index(25, 6, 25 * 6 - 1);
+        assert_eq!(x + 1, 25);
+        assert_eq!(y + 1, 6);
+    }
+
 }
